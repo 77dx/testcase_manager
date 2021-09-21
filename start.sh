863d25a7aa881a3bd18a3eabb6469f0969a466b8
77dx <396321556@qq.com>
2021-09-21 16:11:56 +0800

    fix bugs

--- start.sh
+++ start.sh
@@ -4,5 +4,7 @@ if [ $ProcNumber -el 0 ];then
     echo "Django is not run"
     nohup python3 manage.py runserver 0.0.0.0:8000 >django.log 2>&1 &
 else
-    echo "Django is running..."
+    kill -9 $(ps -ef |grep python | grep -v grep | awk '{print $2}')
+    nohup python3 manage.py runserver 0.0.0.0:8000 >django.log 2>&1 &
+    echo "Django is restarting...，"
 fi
